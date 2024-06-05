0d52f6ca7f980f989cb197e0027d4856a8a9600c
parshya <prashsingh1997@gmail.com>
2024-06-05 18:10:23 +0530

    Pass lambda expression for sowing result

--- src/main/java/org/example/IMathFunction.java
+++ src/main/java/org/example/IMathFunction.java
@@ -4,4 +4,9 @@ package org.example;
 public interface IMathFunction {
 
     int calculate(int a,int b);
+
+    static void printResult(int a,int b,String funtion , IMathFunction obj)
+    {
+        System.out.println("Result of "+ funtion+" is "+obj.calculate(a,b));
+    }
 }
--- src/main/java/org/example/LambdaExpressionExample.java
+++ src/main/java/org/example/LambdaExpressionExample.java
@@ -18,6 +18,12 @@ public class LambdaExpressionExample {
         System.out.println(add.calculate(6,5));
         System.out.println(multiply.calculate(6,5));
         System.out.println(divide.calculate(6,3));
+
+        //Passing lambda expression as funtion paramter to print result using static funtion
+
+        IMathFunction.printResult(2,5,"Addtion",add);
+        IMathFunction.printResult(2,5,"Multiply",multiply);
+        IMathFunction.printResult(10,5,"Division",divide);
     }
 
 
