264a30083cb199add9ca910adedf1eca544d2d28
parshya <prashsingh1997@gmail.com>
2024-06-08 11:23:46 +0530

    updated PrintDouble class

--- src/main/java/org/example/PrintDouble.java
+++ src/main/java/org/example/PrintDouble.java
@@ -11,7 +11,16 @@ public class PrintDouble {
 
         List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5);
 
+        Function<Integer,Double> doubleFunction= integer -> integer.doubleValue();
+       //or
+        /*Function<Integer,Double> doubleFunction= Integer::doubleValue;*/
+
+        numbers.forEach(n->{
+            System.out.println("Mth6: Foreach lambda double value: "+doubleFunction.apply(n));
+        });
+
         numbers.forEach(n-> System.out.println(n*2.0));
+
 
         Consumer<Integer> myListAction=n->{
             System.out.println("Using Consumer funtional interface : "+n*2.0);
